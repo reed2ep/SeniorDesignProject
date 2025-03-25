cb64e4794062c7e1d93ffa55efd63024ac34e42f
Oankar302 <ghorpaos@mail.uc.edu>
2025-03-25 15:13:33 -0400

    Ui changes

--- RockClimber/AppShell.xaml.cs
+++ RockClimber/AppShell.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace RockClimber
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+
+namespace RockClimber
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+        }
+
+        private async void OnHelpClicked(object sender, EventArgs e)
+        {
+            string url = "https://github.com/reed2ep/SeniorDesignProject"; // Replace with your actual help page URL
+            await Launcher.OpenAsync(new Uri(url));
         }
     }
 }
